203644b2c49d34e5fcecad895ae307cf2bc135e1
zeynepbalibek <97685101+zeynepbalibek@users.noreply.github.com>
2022-05-25 11:25:56 +0300

    Add files via upload

---
+++ logic-1/inOrder.js
@@ -0,0 +1,11 @@
+/* Given three ints, a b c, return true if b is greater than a, and c is greater than b.
+ * However, with the exception that if "bOk" is true, b does not need to be greater than a.
+ */
+
+function inOrder(a, b, c, bOk) {
+    if (bOk == true) {
+        return c > b;
+    } else {
+        return b > a && c > b;
+    }
+}
