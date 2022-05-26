bc96bfe456ea7b66f7bff496d87b5f0435861df0
zeynepbalibek <97685101+zeynepbalibek@users.noreply.github.com>
2022-05-26 13:40:08 +0300

    Add files via upload

---
+++ logic-1/withoutDoubles.js
@@ -0,0 +1,14 @@
+/* Return the sum of two 6-sided dice rolls, each in the range 1..6. However, if noDoubles is
+ * true, if the two dice show the same value, increment one die to the next value, wrapping
+ * around to 1 if its value was 6.
+ */
+
+function withoutDoubles(die1, die2, noDoubles) {
+    if (noDoubles && die1 == die2) {
+        die1++;
+        if (die1 == 7)
+            die1 = 1;
+    }
+
+    return die1 + die2;
+}
